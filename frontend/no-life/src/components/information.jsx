13c7237ee44c3dbfa78f35837b82fd1f1046dbd1
djametcode <kampretsiji1@gmail.com>
2023-07-01 13:25:04 +0700

    changes

--- frontend/no-life/src/components/information.jsx
+++ frontend/no-life/src/components/information.jsx
@@ -3,8 +3,8 @@ const Information = () => {
     <>
       <div className="collapse bg-base-200">
         <input type="checkbox" />
-        <div className="collapse-title text-base">What is No-Life?</div>
-        <div className="collapse-content">
+        <div className="collapse-title text-sm">What is No-Life?</div>
+        <div className="collapse-content text-xs">
           <p>
             Generaly No-Life known as condition where you fell not live. You
             dont act like life people. letting yourself in a cage
@@ -13,10 +13,10 @@ const Information = () => {
       </div>
       <div className="collapse bg-base-200">
         <input type="checkbox" />
-        <div className="collapse-title text-base">
+        <div className="collapse-title text-sm">
           What is the purpose of this app?
         </div>
-        <div className="collapse-content">
+        <div className="collapse-content text-xs">
           <p>
             If you fell lonely, This app letting you to get a online friends.
           </p>
@@ -24,8 +24,8 @@ const Information = () => {
       </div>
       <div className="collapse bg-base-200 mb-16">
         <input type="checkbox" />
-        <div className="collapse-title text-base">I am fell depressed ?</div>
-        <div className="collapse-content">
+        <div className="collapse-title text-sm">I am fell depressed ?</div>
+        <div className="collapse-content text-xs">
           <p>
             Open this app and click sign up. Get friend as soon as possible :)
           </p>
