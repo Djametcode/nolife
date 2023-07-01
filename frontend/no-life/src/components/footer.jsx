a17056ee99f57bc206f02e57df1fa4d1293e3e9f
djametcode <kampretsiji1@gmail.com>
2023-07-01 12:59:29 +0700

    changes

--- frontend/no-life/src/components/footer.jsx
+++ frontend/no-life/src/components/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div className=" h-14 bg-neutral flex justify-start items-center text-white font-geologica pl-5 text-lg">
+    <div className=" max-sm:h-10 max-sm:text-sm h-14 bg-neutral flex justify-start items-center text-white font-geologica pl-5 text-lg">
       <p>&copy; copyright {year} | Djamet Coder</p>
     </div>
   );
