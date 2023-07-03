c3ea6dbcbdca053dc9a1f067c2d4e9302edff58c
Djamet Code <kampretsiji1@gmail.com>
2023-07-04 02:44:16 +0700

    changes

--- frontend/no-life/src/handler/getCurrentUser.js
+++ frontend/no-life/src/handler/getCurrentUser.js
@@ -5,7 +5,7 @@ const getCurrentUser = async () => {
   const userId = Cookies.get("userId");
   try {
     const response = await axios.get(
-      `https://wandering-undershirt-dog.cyclic.app/api/v11/no-life/get-current-user/${userId}`
+      `http://localhost:3000/api/v11/no-life/get-current-user/${userId}`
     );
     const result = await response.data;
     console.log(result);
