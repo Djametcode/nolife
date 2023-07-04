a0a60a6212e94fb5c775bf1db91c214a5a845d47
Djamet Code <kampretsiji1@gmail.com>
2023-07-05 02:14:20 +0700

    changes

--- frontend/no-life/src/components/myPost.jsx
+++ frontend/no-life/src/components/myPost.jsx
@@ -6,7 +6,7 @@ const MyPost = () => {
   //grid grid-rows-[175px_175px_175px_175px_175px_175px_175px] grid-flow-row-dense grid-cols-[1fr_1fr]
 
   return (
-    <div className=" max-sm:pt-24 max-sm:pb-24">
+    <div className=" max-sm:pt-24 max-sm:pb-24 font-geologica">
       <h1 className=" p-2">My Post</h1>
       <hr />
 
