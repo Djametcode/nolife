ed87513c65878f1aa9411dd2429c6773ca978adc
Djamet Code <kampretsiji1@gmail.com>
2023-07-05 01:32:39 +0700

    changes

--- frontend/no-life/src/handler/loginHandler.js
+++ frontend/no-life/src/handler/loginHandler.js
@@ -15,9 +15,9 @@ const loginHandler = async (event, item, navigate, dispatch) => {
     await Cookies.set("token", token);
     await Cookies.set("userId", user._id);
     await dispatch(authAction.login());
-    const item = setInterval(() => {
+    const intervals = setInterval(() => {
       navigate("/welcome");
-      clearInterval(item);
+      clearInterval(intervals);
     }, 3000);
     console.log(datas);
   } catch (error) {
