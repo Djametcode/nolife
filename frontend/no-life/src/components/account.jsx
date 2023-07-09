02d5eb58fcaeeb6f4a33e1a21592e7964b6a70c1
djametcode <kampretsiji1@gmail.com>
2023-07-09 19:35:06 +0700

    changes

--- frontend/no-life/src/components/account.jsx
+++ frontend/no-life/src/components/account.jsx
@@ -15,7 +15,7 @@ const Account = () => {
   const getCurrentUser = async () => {
     try {
       const response = await axios.get(
-        `http://localhost:3000/api/v11/no-life/get-current-user/${userId}`,
+        `https://wandering-undershirt-dog.cyclic.app/api/v11/no-life/get-current-user/${userId}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
