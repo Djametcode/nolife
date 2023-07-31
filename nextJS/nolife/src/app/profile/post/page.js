eb79820fd353b10e2224b133ef469213e813283c
djametcode <kampretsiji1@gmail.com>
2023-07-31 21:49:04 +0700

    changes

--- nextJS/nolife/src/app/profile/post/page.js
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useSelectedLayoutSegments } from "next/navigation";
-
-export default function PostListComponent() {
-  const segment = useSelectedLayoutSegments();
-  return <div>{segment}</div>;
-}
