88ba1d9b19e5bfb2533af082bbbb1b4b8277fd5c
djametcode <kampretsiji1@gmail.com>
2023-09-09 17:17:42 +0700

    changes

--- frontend-tsc/nolife/src/component/navbar.tsx
+++ frontend-tsc/nolife/src/component/navbar.tsx
@@ -29,16 +29,16 @@ export default function Navbar() {
   }, []);
   return (
     <div className=" flex justify-around p-5 w-full h-14 items-center bg-slate-50">
-      <Link className=" w-full" href={"/landing"}>
+      <Link className=" w-full flex items-center" href={"/landing"}>
         {path === "/landing" ? <GoHomeFill size={25} /> : <GoHome size={25} />}
       </Link>
-      <Link className=" w-full" href={"/"}>
+      <Link className=" w-full flex items-center" href={"/"}>
         <GoSearch size={25} />
       </Link>
-      <Link className=" w-full" href={"/"}>
+      <Link className=" w-full flex items-center" href={"/"}>
         <BsPlusSquare size={25} />
       </Link>
-      <Link className=" w-full" href={"/"}>
+      <Link className=" w-full flex items-center" href={"/"}>
         <BiMoviePlay size={25} />
       </Link>
       <div>
